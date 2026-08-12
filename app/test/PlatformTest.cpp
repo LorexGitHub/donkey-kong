551bdcb14f617f74bfa8fe998970ecf4ae1302fc
LorexHub <77957054+LorexHub@users.noreply.github.com>
2026-08-12 13:22:27 +0200

    added platformtest and relations diagrams

--- app/test/PlatformTest.cpp
+++ app/test/PlatformTest.cpp
@@ -14,6 +14,23 @@ TEST(PlatformTest, SetSolidCreatesHole) {
     EXPECT_TRUE(p.is_solid(4));
 }
 
+TEST(PlatformTest, SetSolidTogglesState) {
+    Platform p(10.f, 20.f, 500.f);
+    p.set_solid(7, false);
+    EXPECT_FALSE(p.is_solid(7));
+    p.set_solid(7, true);
+    EXPECT_TRUE(p.is_solid(7));
+}
+
+TEST(PlatformTest, SetSolidOutOfBoundsIgnored) {
+    Platform p(0, 0, 100);
+    p.set_solid(-1, false);
+    p.set_solid(10, false);
+    p.set_solid(100, false);
+    for (int i = 0; i < 10; i++)
+        EXPECT_TRUE(p.is_solid(i));
+}
+
 TEST(PlatformTest, BoundsMatchParameters) {
     Platform p(50, 100, 700);
     auto b = p.get_bounds();
